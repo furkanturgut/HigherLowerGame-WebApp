4b6704256e527d66535d49eb9c42a74efaf6051d
Furkan Turgut <153141827+furkanturgut@users.noreply.github.com>
2025-03-26 00:11:52 +0300

    Create VsCircle.tsx

---
+++ frontend/src/Components/VsCircle/VsCircle.tsx
@@ -0,0 +1,20 @@
+const VsCircle = () => {
+  return (
+    <div className="
+      w-20 
+      h-20 
+      bg-[#FF9500] 
+      rounded-full 
+      flex 
+      items-center 
+      justify-center 
+      text-white 
+      font-bold 
+      text-2xl
+    ">
+      VS
+    </div>
+  );
+};
+
+export default VsCircle;
